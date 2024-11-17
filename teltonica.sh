b371af3d470972918dcbb38bcfa3362dc26914a6
Efthymios Tserepas <156537892+efthymios-tserepas@users.noreply.github.com>
2024-11-17 19:21:16 +0200

    teltonica.sh

--- teltonica.sh
+++ teltonica.sh
@@ -254,9 +254,8 @@ download_sdk() {
     # Execute the feeds for OpenWrt
     echo "Executing the feeds for OpenWrt..."
     ./scripts/feeds update -a
-    # Commented out lrexlib installation since it may not be available
     ./scripts/feeds install libffi lrexlib
-    #./scripts/feeds install libffi
+    ./scripts/feeds install -a
 
     # Additional steps for ccache
     echo "Creating folder for ccache and downloading the package..."
